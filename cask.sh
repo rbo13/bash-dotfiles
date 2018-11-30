f752ad321d44d95a086a2ed5b66eaa8593b34ae5
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-11-30 21:29:44 +0800

    Added cask.sh to install apps using brew cask
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

---
+++ cask.sh
@@ -0,0 +1,22 @@
+# Make sure we update homebrew
+brew update
+
+# Upgrade necessary tools
+brew upgrade
+
+# Browsers
+# brew cask install google-chrome-canary
+brew cask install firefox
+
+# Install utils app
+brew cask install dash
+brew cask install insomnia
+brew cask install sequel-pro
+
+# Install productivity
+brew cask install slack
+brew cask install spotify
+
+# Others
+brew cask install transmission
+brew cask install vlc
