9fc7a2d313d93529a4f411f0862ded2ed355ed27
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-04-30 20:39:48 +0800

    Added amethyst for tiling manager
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- cask.sh
+++ cask.sh
@@ -18,6 +18,7 @@ brew cask install dash
 brew cask install insomnia
 brew cask install sequel-pro
 brew cask install keybase
+brew cask install amethyst
 
 # Install productivity
 brew cask install slack
